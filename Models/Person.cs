a847ca6ccadf94c5e575b4479315bc7b18a71808
romaintrnbx <romain.troonbeeckx@gmail.com>
2022-12-03 10:11:22 +0100

    Base person

--- Models/Person.cs
+++ Models/Person.cs
@@ -7,6 +7,21 @@ namespace CSharpRPG.Models
 {
     public class Person
     {
-        
+        public Person(string pseudo)
+        {
+            Pseudo = pseudo;
+            Live = 10; 
+            View = "☺";
+        }
+
+        public string Pseudo { get; set; }
+        public double Live {get; private set;}
+        public string View { get; private set; }
+    
+    public void ViewLives (int live){
+        string viewlives = new string('♥', live);
+            Console.WriteLine(viewlives);
+    }
+
     }
 }
