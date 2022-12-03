d75b24af4d191ab2a57aa1dbf6a4dd9bf09266ed
romaintrnbx <romain.troonbeeckx@gmail.com>
2022-12-03 08:56:07 +0100

    base

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-
-// Hello ceci est un test !
-
-// on est dans la merde, force à nous
 // .gitignore
