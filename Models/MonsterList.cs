e157b9e86fdd626fb76d8b39af79e01a524e8bff
MauryTiteux <maury.titeux@gmail.com>
2022-12-10 11:05:15 +0100

    woman

--- Models/MonsterList.cs
+++ Models/MonsterList.cs
@@ -5,6 +5,6 @@ public static class MonsterList
 {
     public static Monster Zombie => new Monster(RandomUtils.GetRandomBetween(9, 10), "🧟", WeaponList.Claw);
     public static Monster Skeleton => new Monster(RandomUtils.GetRandomBetween(8, 9),"💀", WeaponList.RustedSword);
-    public static Monster Femme  => new Monster(RandomUtils.GetRandomBetween(9, 10),"♀", WeaponList.RustedSword);
+    public static Monster Femme  => new Monster(RandomUtils.GetRandomBetween(9, 10),"👩", WeaponList.RustedSword);
 
 }
