cce41b966a0c496ad913ec7820e495d86cefb024
Nicolas Thienpont <73384954+NThienpont@users.noreply.github.com>
2023-01-12 15:45:59 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
-﻿// .gitignore
+// .gitignore
 using CSharpRPG.Models;
 
+bool fightingStatus = true;
 Hero Player = new Hero ("Romain",10,"🤴", WeaponList.MasterSword);
+Monster Monstre = new Monster(5,"",WeaponList.Claw);
 
 /*
 Player.ViewLifes();
@@ -11,5 +13,26 @@ Player.AddLifes(20);
 Player.ViewLifes();
 Console.WriteLine(Player);*/
 
+// Test interface + map
+
+Interface interface1 = new Interface(Player);
+interface1.top();
+
 Matrix Matrix1 = new Matrix();
 Matrix1.ViewMap(Player);
+
+interface1.bottom();
+interface1.aideCombat();
+
+// Test des options de combat
+
+Fight fight1 = new Fight(Player, Monstre);
+fight1.violentAttack();
+fight1.attack();
+fight1.heal();
+fight1.escape();
+
+
+// Permettrait de voir si un combat est en cours et changer l'interface en fonction, je dois encore réfléchir exactement comment
+
+Console.WriteLine(fightingStatus);
